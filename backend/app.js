f4c578b65043bc88a3dab88dd2450d65993ce51e
ProjetsDevFanny <contact.fanny.simon@gmail.com>
2026-01-11 23:16:30 +0100

    Fix CORS headers to allow frontend on Netlify to access backend

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.setHeader(
     'Access-Control-Allow-Origin',
-    'https://grimoire-ocr-fanny.netlify.app'
+    'https://mon-vieux-grimoire-ocr-fs.netlify.app/'
   ); 
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
